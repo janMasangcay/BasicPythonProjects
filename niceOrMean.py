125d6955491f33d3e094bd8a3a99629c245cf48c
Jan Masangcay <jancdm@hotmail.com>
2019-05-05 11:58:19 -0700

    Add comment explain what getName() does

--- niceOrMean.py
+++ niceOrMean.py
@@ -14,8 +14,20 @@
 def start(nice=0, mean=0, name=""):
     # get user's name
     name = getName(name)
+    nice, mean, name = niceMean(nice, mean, name)
+    print(nice)
+    print(mean)
+    print(name)
 
 def getName(userName):
+    """
+        Check if this is a new game or not.
+        If it is new, get the user's name.
+        If it is not a new game, thank the player for
+        playing again and continue with the game.
+    """
+    # meaning, if we do not already have this user's name,
+    # then they are a new player and need to get their name
     if userName != "":
         print('\nThank you for playing again, {}!'.format(userName))
     else:
@@ -28,6 +40,8 @@ def getName(userName):
                 print('but at the end of the game your fate \nwill be sealed by your actions.')
                 go = False
     return userName   
+
+
 
 
 
